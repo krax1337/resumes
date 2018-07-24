4d260129530dbbcef46784611e76665a348ded2a
Asanali <asanali1@mail.ru>
2018-07-24 22:20:14 +0600

    heroku deploy

--- simpleform/settings.py
+++ simpleform/settings.py
@@ -12,8 +12,12 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import django_heroku
+
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -100,3 +104,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
