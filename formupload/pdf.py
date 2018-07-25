24bb7ccc5cfb21066b15e0c078a16d2432bdddd8
Asanali <asanali1@mail.ru>
2018-07-25 13:22:06 +0600

    heroku_23

--- formupload/pdf.py
+++ formupload/pdf.py
@@ -7,7 +7,7 @@ import urllib.request
 from xml.etree import ElementTree as ET
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
-import ntlk
+import nltk
 import string
 from lxml import etree
 import html
